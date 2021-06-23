90a3a5717cac5cb2bab88dcc7dbb7d49a0dad006
chaz6chez <250220719@qq.com>
2021-06-23 11:39:22 +0800

    修复路由魔术方法

--- src/Router.php
+++ src/Router.php
@@ -6,9 +6,9 @@ namespace Kernel;
 /**
  * 该路由简单实现了JsonRpc协议的两种方式
  *
- * @method static Router notice(string $route, callable $callback)
- * @method static Router normal(string $route, callable $callback)
- * @method static Router any(string $route, callable $callback)
+ * @method static Route notice(string $route, callable $callback)
+ * @method static Route normal(string $route, callable $callback)
+ * @method static Route any(string $route, callable $callback)
  *
  * @example Router.php 33 42 继承重写可适配如HTTP等其他协议
  * @author chaz6chez <250220719@qq.com>
